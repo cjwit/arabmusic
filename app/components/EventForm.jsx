c7923866a32d972a140a353e4d506cfe94559ebe
cjwit <chris.witulski@gmail.com>
2016-04-21 12:30:22 -0400

    add event store and action, dispatcher

--- app/components/EventForm.jsx
+++ app/components/EventForm.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var actions = require('../actions/EventActions');
 
 module.exports = React.createClass({
     getInitialState: function() {
@@ -11,6 +12,18 @@ module.exports = React.createClass({
         };
     },
 
+    addEvent: function(e) {
+        e.preventDefault();
+        actions.addEvent(this.state);
+    },
+
+    handleInputChange: function(e) {
+        e.preventDefault();
+        var name = e.target.name;
+        var state = this.state;
+        state[name] = e.target.value;
+        this.setState(state);
+    },
 
     render: function() {
         return (
@@ -20,21 +33,42 @@ module.exports = React.createClass({
                     <div className = 'col-md-8 col-md-offset-2 holder'>
                         <form>
                             <div className="form-group">
-                                <label HTMLfor="eventTitle">Title of the event</label>
-                                <input type="text" className="form-control" id="eventTitle" placeholder="Title" />
+                                <label className = 'control-label' HTMLfor="name">Title of the event</label>
+                                <input type="text" className="form-control"
+                                       id="name"
+                                       name = 'name'
+                                       placeholder="Title"
+                                       value = { this.state.name }
+                                       onChange = { this.handleInputChange } />
                             </div>
                             <div className="form-group">
-                                <label HTMLfor="eventLocation">City, State or Province, and Country</label>
+                                <label className = 'control-label' HTMLfor="location">City, State or Province, and Country</label>
                                 <p className="help-block">You can give more specific information in the details below</p>
-                                <input type="text" className="form-control" id="eventLocation" placeholder="City" />
+                                <input type="text" className="form-control"
+                                       id="location"
+                                       name = 'location'
+                                       placeholder="City"
+                                       value = { this.state.location }
+                                       onChange = { this.handleInputChange } />
                             </div>
                             <div className="form-group">
-                                <label HTMLfor="eventDate">Date of the event</label>
-                                <input type="text" className="form-control" id="eventDate" placeholder="Date" />
+                                <label className = 'control-label' HTMLfor="date">Date of the event</label>
+                                <input type="text" className="form-control"
+                                       id="date"
+                                       name = 'date'
+                                       placeholder="Date"
+                                       value = { this.state.date }
+                                       onChange = { this.handleInputChange } />
+
                             </div>
                             <div className="form-group">
-                                <label HTMLfor="eventDescription">Some information, including a contact for anyone who has more questions</label>
-                                <textarea className="form-control" rows = "3" id="eventDescription" placeholder="Details and contact information" />
+                                <label className = 'control-label' HTMLfor="description">Some information, including a contact for anyone who has more questions</label>
+                                <textarea className="form-control" rows = "3"
+                                          id="description"
+                                          name = 'description'
+                                          placeholder="Details and contact information"
+                                          value = { this.state.description }
+                                          onChange = { this.handleInputChange } />
                             </div>
                             <button type="submit" className="btn btn-default">Submit</button>
                         </form>
