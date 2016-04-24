bfc3f230f505a4d2c2637ade0a1fbe70fd86b3ba
cjwit <chris.witulski@gmail.com>
2016-04-24 13:45:00 -0400

    add header to discussionPage

--- app/components/DiscussionPage.jsx
+++ app/components/DiscussionPage.jsx
@@ -16,10 +16,16 @@ module.exports = React.createClass({
 
         return (
             <div>
-                <div className = 'container'>
+                <div id = 'head' className = 'row'>
+                    <div className = 'col-md-8 col-md-offset-2 holder'>
+                        <h1>Questions and Answers</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+                <div className = 'container'>    
                     <div className = 'row holder'>
                         <div className = 'col-md-8'>
-                            <h1>Recent Posts</h1>
+                            <h1>Post Details</h1>
                             <Discussion info = { info } />
                         </div>
                         <div className = 'col-md-4'>
