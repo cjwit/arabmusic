6c0fff17b861f945fa717f0dfd3e3270acd6b1e2
cjwit <chris.witulski@gmail.com>
2016-04-28 11:00:49 -0400

    edit event date and tags working

--- app/components/Event.jsx
+++ app/components/Event.jsx
@@ -1,7 +1,23 @@
 var React = require('react');
 var actions = require('../actions/EventActions');
+var tags = require('../tags.js');
 
 module.exports = React.createClass({
+    getInitialState: function() {
+        return ({
+            editing: false,
+            info: {
+                name: this.props.info.name,
+                location: this.props.info.location,
+                description: this.props.info.description,
+                date: new Date(this.props.info.date),
+                tags: this.props.info.tags,
+                owner: this.props.info.owner,
+                id: this.props.info._id
+            }
+        })
+    },
+
     deleteEvent: function(e) {
         e.preventDefault();
         actions.deleteEvent(this.props.info)
@@ -14,17 +30,76 @@ module.exports = React.createClass({
         if (id != null) {
             window.location.href = '/' + folder;
         }
+    },
 
+    openForm: function() {
+        this.setState({ editing: true })
+        var info = this.state.info
+
+        // wait for rendering to complete
+        var toggleTag = this.toggleTag;
+        window.requestAnimationFrame(function() {
+            // set up form
+            $('#tags :input').change(function() {
+                toggleTag(this.name);
+            })
+
+            $(function () {
+                var datePicker = $('#date');
+                datePicker.datetimepicker({
+                    format: "MM/DD/YYYY"
+                })
+                datePicker.data('DateTimePicker').date(info.date)
+            });
+        })
+    },
+
+    closeForm: function(e) {
+        e.preventDefault();
+        this.setState({ editing: false });
+        console.log('editing state', this.state.editing)
+    },
+
+    toggleTag: function(name) {
+        console.log(name)
+        var info = this.state.info;
+        var index = info.tags.indexOf(name)
+        if (index === -1) {
+            info.tags.push(name);
+        } else {
+            info.tags.splice(index, 1);
+        }
+        this.setState({
+            info: info
+        })
+    },
+
+    handleInputChange: function(e) {
+        e.preventDefault();
+        var name = e.target.name;
+        if (name === 'date') {
+            var value = new Date(e.target.value);
+        } else {
+            var value = e.target.value;
+        }
+
+        var info = this.state.info;
+        info[name] = value;
+
+        this.setState({
+            info: info
+        });
     },
 
     editEvent: function(e) {
         e.preventDefault();
+        console.log('edit', this.state.info);
         // actions.editEvent(this.props.info)
     },
 
     render: function() {
-        var info = this.props.info;
-        var id = info._id;
+        var info = this.state.info;
+        var id = info.id;
         var eventPage = Boolean(window.location.pathname.match(/^\/events\//));
         var tagString = '';
         info.tags.map(function(tag, index) {
@@ -35,47 +110,118 @@ module.exports = React.createClass({
             }
         })
 
-        return (
-            <div className = 'event' id = { id }>
-                <span className = 'event-name'>
-                    { info.name }:&nbsp;
-                </span>
+        if (!this.state.editing) {
+            return (
+                <div className = 'event' id = { id }>
+                    <span className = 'event-name'>
+                        { info.name }:&nbsp;
+                    </span>
 
-                { info.location ? <span className = 'event-place'>{ info.location },&nbsp;</span> : null }
+                    { info.location ? <span className = 'event-place'>{ info.location },&nbsp;</span> : null }
 
-                <span className = 'event-date'>
-                    { info.date.toLocaleDateString() }
-                </span>
-                    { !eventPage ?
-                    <div className = 'btn-group pull-right' role = 'group' aria-label='...'>
+                    <span className = 'event-date'>
+                        { info.date.toLocaleDateString() }
+                    </span>
+                        { !eventPage ?
+                        <div className = 'btn-group pull-right' role = 'group' aria-label='...'>
 
-                        <a href = { '/events/' + id } role = 'button' className = 'btn btn-default'>
-                            <span className="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>
-                        </a>
+                            <a href = { '/events/' + id } role = 'button' className = 'btn btn-default'>
+                                <span className="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>
+                            </a>
+                        </div>
+                            :
+                        <div className = 'btn-group pull-right' role = 'group' aria-label='...'>
+                            <a onClick = { this.openForm } role = 'button' className = 'btn btn-default'>
+                                <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            </a>
+                            <a onClick = { this.deleteEvent } role = 'button' className = 'btn btn-default'>
+                                <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                            </a>
+                        </div>
+                    }
+                    <br />
+
+                    <div className = 'event-description'>
+                        { info.description }
                     </div>
-                        :
-                    <div className = 'btn-group pull-right' role = 'group' aria-label='...'>
-                        <a onClick = { this.editEvent } role = 'button' className = 'btn btn-default'>
-                            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                        </a>
-                        <a onClick = { this.deleteEvent } role = 'button' className = 'btn btn-default'>
-                            <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                        </a>
-                    </div>
-                }
-                <br />
-
-                <div className = 'event-description'>
-                    { info.description }
+                    { info.tags.length > 0 ?
+                        <div className = 'event-tags'>
+                            <span className = 'glyphicon glyphicon-tag' aria-hidden = 'true'></span>&nbsp;
+                            { tagString }
+                        </div>
+                        : null
+                    }
                 </div>
-                { info.tags.length > 0 ?
-                    <div className = 'event-tags'>
-                        <span className = 'glyphicon glyphicon-tag' aria-hidden = 'true'></span>&nbsp;
-                        { tagString }
-                    </div>
-                    : null
-                }
-            </div>
             )
+        } else {
+
+            var tagButtons = [];
+            var allTags = tags.geographic.concat(tags.musical).concat(tags.conceptual);
+            allTags.map(function(tag, index) {
+                var preChecked = (info.tags.indexOf(tag) !== -1)
+                if (preChecked) console.log(preChecked, tag);
+                tagButtons.push(
+                    <label className = { preChecked ? 'tag btn btn-default btn-xs active' : 'tag btn btn-default btn-xs' }
+                           onChange = { this.toggleTag }
+                           key = { 'check' + tag }>
+                        <input type = 'checkbox'
+                               name = { tag }
+                               autocomplete='off'
+                               defaultChecked = { preChecked } /> { tag }
+                    </label>)
+            });
+            return (
+                <div className = 'event' id = { id }>
+                    <form onSubmit = { this.editEvent } id = 'editEventForm'>
+                        <div className="form-group">
+                            <label className = 'control-label' HTMLfor="name">Title of the event</label>
+                            <input type="text" className="form-control"
+                                   id="name"
+                                   name = 'name'
+                                   placeholder="Title"
+                                   defaultValue = { info.name }
+                                   onChange = { this.handleInputChange } />
+                        </div>
+                        <div className="form-group">
+                            <label className = 'control-label' HTMLfor="location">City, State or Province, and Country</label>
+                            <input type="text" className="form-control"
+                                   id="location"
+                                   name = 'location'
+                                   placeholder="City"
+                                   defaultValue = { info.location }
+                                   onChange = { this.handleInputChange } />
+                            <p className="help-block">Use the description to provide more details</p>
+                        </div>
+                        <div className="form-group">
+                            <label className = 'control-label' HTMLfor="date">Date of the event</label>
+                            <input type="text" className="form-control"
+                                   id="date"
+                                   name = 'date'
+                                   onBlur = { this.handleInputChange } />
+                            <p className="help-block">Use the description to provide times</p>
+                        </div>
+                        <div className="form-group">
+                            <label className = 'control-label' HTMLfor="description">Some information, including a contact for anyone who has more questions</label>
+                            <textarea className="form-control" rows = "3"
+                                      id="description"
+                                      name = 'description'
+                                      placeholder="Details and contact information"
+                                      defaultValue = { info.description }
+                                      onChange = { this.handleInputChange } />
+                        </div>
+                        <div className="form-group">
+                            <label className = 'control-label'>Select Tags</label>
+                            <div id = 'tags' class = 'btn-group' data-toggle='buttons'>
+                                { tagButtons }
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn btn-default">Submit</button>&nbsp;
+                        <button className="btn btn-danger" onClick = { this.closeForm }>Cancel</button>
+                    </form>
+                </div>
+            )
+        }
+
     }
 })
