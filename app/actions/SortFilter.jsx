a64274bbbe74d1810e1082378b49dcd611741684
cjwit <chris.witulski@gmail.com>
2016-05-04 18:14:30 -0400

    sorting and filtering working for resources and notices

---
+++ app/actions/SortFilter.jsx
@@ -0,0 +1,78 @@
+module.exports = {
+    sortByEdit: function(list) {
+        var sorted = list.sort(function(a, b) {
+            return new Date(b.editDate) - new Date(a.editDate);
+        });
+        return sorted;
+    },
+
+    sortByEventDate: function(list) {
+        var sorted = list.sort(function(a, b) {
+            return new Date(a.eventDate) - new Date(b.eventDate);
+        });
+        return sorted;
+    },
+
+    sortByName: function(list) {
+        var sorted = list.sort(function(a, b) {
+            var nameA = a.name.toUpperCase();
+            var nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            } else if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+        return sorted;
+    },
+
+    sortByTitle: function(list) {
+        var sorted = list.sort(function(a, b) {
+            var titleA = a.title.toUpperCase();
+            var titleB = b.title.toUpperCase();
+            if (titleA < titleB) {
+                return -1;
+            } else if (titleA > titleB) {
+                return 1;
+            }
+            return 0;
+        });
+        return sorted;
+    },
+
+    getSorted: function(list, sortBy) {
+        var sorted;
+        switch (sortBy) {
+            case "edit":
+                sorted = this.sortByEdit(list);
+                break;
+            case "title":
+                sorted = this.sortByTitle(list);
+                break;
+            case "name":
+                sorted = this.sortByName(list);
+                break;
+            case "eventDate":
+                sorted = this.sortByEventDate(list);
+                break;
+        }
+        return sorted;
+    },
+
+    filterBy: function(list, tags) {
+        if (tags.length === 0) {
+            return list;
+        }
+        var filtered = list.filter(function(item) {
+            var keep = false;
+            item.tags.forEach(function(tag) {
+                if (tags.indexOf(tag) !== -1) {
+                    keep = true;
+                }
+            })
+            return keep;
+        })
+        return filtered;
+    }
+}
