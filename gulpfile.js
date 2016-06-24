9e2466d3f4f563411bdeb4aa27f8c3f3e0ebfa9c
cjwit <chris.witulski@gmail.com>
2016-06-23 20:50:19 -0400

    concat and minify scrips and css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var concatCSS = require('gulp-concat-css');
+var uglifyCSS = require('gulp-uglifycss')
+
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
-var sass = require('gulp-sass');
 
 // creates main.js
 gulp.task('bundle', function() {
@@ -12,40 +17,50 @@ gulp.task('bundle', function() {
     }).transform(reactify)
         .bundle()
         .pipe(source('main.js'))
-        .pipe(gulp.dest('app/dist'))
+        .pipe(gulp.dest('app'))
 })
 
 // convert sass to css
 gulp.task('styles', function() {
     gulp.src('app/*.sass')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/dist'));
+        .pipe(gulp.dest('app/styles'));
 });
 
-// watch sass conversion
+// watch sass conversion, run 'styles' on changes
 gulp.task('watch-sass', function() {
     gulp.watch('app/*.sass', ['styles'])
 });
 
-// moves the static files into app/dist
-gulp.task('copy-libraries', function() {
-    return gulp.src(['app/lib/bootstrap/dist/css/bootstrap.min.css',
-                     'app/lib/bootstrap/dist/js/bootstrap.min.js',
-                     'app/lib/jquery/dist/jquery.min.js',
+gulp.task('finalize-scripts', function() {
+    return gulp.src(['app/lib/jquery/dist/jquery.min.js',
                      'app/lib/moment/moment.js',
-                     'app/lib/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
-                     'app/lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'])
+                     'app/lib/bootstrap/dist/js/bootstrap.min.js',
+                     'app/lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+                     'app/main.js'])
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('app/dist'))
+        .pipe(uglify())
         .pipe(gulp.dest('app/dist'));
 });
 
+gulp.task('finalize-css', function() {
+    return gulp.src('app/styles/*.css')
+        .pipe(concatCSS('concatStyles.css'))
+        .pipe(gulp.dest('app/dist'))
+        .pipe(uglifyCSS())
+        .pipe(gulp.dest('app/dist'))
+});
+
 gulp.task('copy', function() {
-    return gulp.src(['app/index.html',
-                     'app/style.css',
-                     'app/social.css'])
+    return gulp.src('app/index.html')
         .pipe(gulp.dest('app/dist'));
 });
 
 gulp.task('default', ['copy',
-                      'bundle'], function() {
+                      'bundle',
+                      'styles',
+                      'finalize-scripts',
+                      'finalize-css'], function() {
     console.log('Gulp completed...');
 });
