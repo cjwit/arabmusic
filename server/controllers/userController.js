0948fca784bdd2e0eb8fd5209ad44ec94458dc53
cjwit <chris.witulski@gmail.com>
2016-12-29 20:48:40 -0500

    add emailing to addUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,6 +6,32 @@ var router = require('express').Router();
 router.route('/:id').post(editUser).delete(deleteUser);
 router.route('/').get(getUsers).post(addUser);
 
+// nodemailer
+var nodemailer = require('nodemailer');
+var smtpConfig = {
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: 'amr.notify@gmail.com',
+		pass: process.env.EMAIL_PASSWORD
+	}
+};
+var transporter = nodemailer.createTransport(smtpConfig);
+transporter.verify(function(err, success) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Email server ready...');
+	}
+})
+
+var mailData = {
+	from: 'amr.notify@gmail.com',
+	to: 'chris.witulski@gmail.com'
+}
+
+// App functions
 function getUsers(req, res) {
     User.find(function (err, users) {
         if (err) res.send(err);
@@ -15,6 +41,13 @@ function getUsers(req, res) {
 
 function addUser(req, res) {
     var user = new User(_.extend({}, req.body));
+
+	mailData.subject = '[AMR] New post';
+	mailData.text = 'New post' + '\r\n' +
+		'Name: ' + user.name + '\r\n' +
+		'Other information may be available on the site.'
+	transporter.sendMail(mailData);
+
     User.save(function (err) {
         if (err) res.send(err);
         else res.json(user);
